de5029a0afab369631a6697306d293144e10ef39
Ganesh252000 <156234237+Ganesh252000@users.noreply.github.com>
2024-03-26 20:54:37 -0500

    added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var jRouter = require('./routes/jackets');
+var gRouter = require('./routes/grid');
+var rRouter = require('./routes/pick');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/jacket', jRouter);
+app.use('/grid', gRouter);
+app.use('/randomitem', rRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
