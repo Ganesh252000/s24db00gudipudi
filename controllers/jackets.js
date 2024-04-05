f1e9ad0569cf83700834a3449d840b2b01fad2af
Ganesh252000 <156234237+Ganesh252000@users.noreply.github.com>
2024-04-05 12:08:10 -0500

    added

--- controllers/jackets.js
+++ controllers/jackets.js
@@ -30,17 +30,17 @@ res.send(`{"error": ${err}}`);
  res.send('NOT IMPLEMENTED: jackets detail: ' + req.params.id);
 };
 // Handle jackets create on POST.
-// Handle Costume create on POST.
+// Handle jackets create on POST.
 exports.jackets_create_post = async function(req, res) {
 console.log(req.body)
 let document = new jackets();
 // We are looking for a body, since POST does not have query parameters.
 // Even though bodies can be in many different formats, we will be picky
 // and require that it be a json object
-// {"costume_type":"goat", "cost":12, "size":"large"}
+// {"jacket_name":"goat", "jacket_brandjacket_brand":12, "cost":"large"}
 document.jacket_name = req.body.jacket_name;
 document.jacket_brand = req.body.jacket_brand;
-document.cost = req.body.cost;
+document.jacket_brandjacket_brand = req.body.jacket_brandjacket_brand;
 try{
 let result = await document.save();
 res.send(result);
@@ -58,4 +58,35 @@ res.send('NOT IMPLEMENTED: jackets delete DELETE ' + req.params.id);
 // Handle jeckets update form on PUT.
 exports.jackets_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: jackets update PUT' + req.params.id);
+};
+// for a specific jackets.
+exports.jackets_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await jackets.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    // Handle jackets update form on PUT.
+exports.jackets_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await jackets.findById( req.params.id)
+// Do updates of properties
+if(req.body.jacket_name)
+toUpdate.jacket_name = req.body.jacket_name;
+if(req.body.jacket_brand) toUpdate.jacket_brand = req.body.jacket_brand;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
