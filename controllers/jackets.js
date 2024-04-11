2ab036e49b66b244217cee3564e0845dcfbe2a68
Ganesh252000 <156234237+Ganesh252000@users.noreply.github.com>
2024-04-10 23:29:59 -0500

    send

--- controllers/jackets.js
+++ controllers/jackets.js
@@ -90,3 +90,68 @@ res.send(`{"error": ${err}: Update for id ${req.params.id}
 failed`);
 }
 };
+// Handle jackets delete on DELETE.
+exports.jackets_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await jackets.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+    // Handle a show one view with id specified by query
+    exports.jackets_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await jackets.findById( req.query.id)
+    res.render('jacketsdetail',
+    { title: 'jackets Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for creating a jackets.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.jackets_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('jacketscreate', { title: 'jackets Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.jackets_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await jackets.findById(req.query.id)
+    res.render('jacketssupdate', { title: 'jackets Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle a delete one view with id from query
+    exports.jackets_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await jackets.findById(req.query.id)
+    res.render('jacketsdelete', { title: 'jackets Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+   
--- routes/jackets.js
+++ routes/jackets.js
@@ -3,4 +3,10 @@ const jackets_controlers= require('../controllers/jackets');
 var router = express.Router();
 /* GET jackets */
 router.get('/', jackets_controlers.jackets_view_all_Page );
+/* GET detail jackets page */
+router.get('/detail', jackets_controlers.jackets_view_one_Page);
+router.get('/create', jackets_controlers.jackets_create_Page);
+/* GET create update page */
+router.get('/update', jackets_controlers.jackets_update_Page);
+router.get('/delete', jackets_controlers.jackets_delete_Page);
 module.exports = router;
