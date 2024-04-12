33e66b9d5e2f1d98bd1feec7c1532754f156651c
Ganesh252000 <156234237+Ganesh252000@users.noreply.github.com>
2024-04-11 20:17:38 -0500

    add

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ var gRouter = require('./routes/grid');
 var rRouter = require('./routes/pick');
 var jackets = require("./models/jackets");
 var resourceRouter =require('./routes/resource');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 var app = express();
 
 // view engine setup
@@ -21,6 +41,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -29,6 +56,14 @@ app.use('/jackets', jRouter);
 app.use('/grid', gRouter);
 app.use('/randomitem', rRouter);
 app.use('/resource', resourceRouter);
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account'));
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -60,7 +95,7 @@ async function recreateDB(){
 await jackets.deleteMany();
 let instance1 = new
 jackets({jacket_name:"Cloth", jacket_brand:'Denim',
-cost:15.4});
+cost:1.4});
 instance1.save().then(doc=>{
 console.log("First object saved")}
 ).catch(err=>{
@@ -68,7 +103,7 @@ console.error(err)
 });
 let instance2 = new
 jackets({jacket_name:"leather", jacket_brand:'Tommy',
-cost:15.4});
+cost:15});
 instance2.save().then(doc=>{
 console.log("First object saved")}
 ).catch(err=>{
@@ -77,7 +112,7 @@ console.error(err)
 
 let instance3 = new
 jackets({jacket_name:"wool", jacket_brand:'H&M',
-cost:15.4});
+cost:14});
 instance3.save().then(doc=>{
 console.log("First object saved")}
 ).catch(err=>{
