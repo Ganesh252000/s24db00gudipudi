fcbfdf5ee6fb88290557b020e190870f41c79fea
Ganesh252000 <156234237+Ganesh252000@users.noreply.github.com>
2024-04-16 23:25:08 -0500

    1

--- models/jackets.js
+++ models/jackets.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 const jacketsSchema = mongoose.Schema({
-    jacket_name: String,
-    jacket_brand: String,
-    cost: Number
+jacket_name: String,
+jacket_brand: {
+    type:String,
+    minlength:1,
+    maxlength:20,
+},
+cost: {
+    type: Number,
+    min:1,
+    max:1000
+}
 })
 module.exports = mongoose.model("jackets",
 jacketsSchema)
--- routes/jackets.js
+++ routes/jackets.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 const jackets_controlers= require('../controllers/jackets');
 var router = express.Router();
 /* GET jackets */
@@ -6,7 +7,22 @@ router.get('/', jackets_controlers.jackets_view_all_Page );
 /* GET detail jackets page */
 router.get('/detail', jackets_controlers.jackets_view_one_Page);
 router.get('/create', jackets_controlers.jackets_create_Page);
-/* GET create update page */
-router.get('/update', jackets_controlers.jackets_update_Page);
+//GET create update page */
+//router.get('/update', jackets_controlers.jackets_update_Page);
+//router.get('/delete', jackets_controlers.jackets_delete_Page);
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+router.post('/login', passport.authenticate('local'), function(req, res) {
+res.redirect('/');
+});
+router.get('/update',secured, jackets_controlers.jackets_update_Page);
 router.get('/delete', jackets_controlers.jackets_delete_Page);
+
 module.exports = router;
+
